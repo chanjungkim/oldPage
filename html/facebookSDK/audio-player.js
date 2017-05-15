09595ce2cab7a9499ba78b59722be2137cb62353
Chanjung Kim <fman1335@gmail.com>
2017-05-16 00:50:31 +0900

    update

---
+++ html/facebookSDK/audio-player.js
@@ -0,0 +1,6 @@
+// Create a new instance of an audio object and adjust some of its properties
+var audio = new Audio();
+audio.src = './html/contents/How_it_Began.mp3';
+audio.controls = true;
+audio.loop = true;
+audio.autoplay = true;
