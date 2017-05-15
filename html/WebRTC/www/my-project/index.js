82a7cd7a63f442ac5bd9e9d249edc3003fa40a32
Chanjung Kim <fman1335@gmail.com>
2017-05-15 16:30:55 +0900

    voice command

---
+++ html/WebRTC/www/my-project/index.js
@@ -0,0 +1,37 @@
+navigator.mediaDevices.getUserMedia({ video: true, audio: true }, function(stream) {
+	
+	var Peer = require('simple-peer')
+	var peer = new Peer({
+		initiator: location.has === '#init',
+		trickle: false,
+		stream: stream
+	})
+
+	peer.on('singnal', function(data){
+		document.getElementById('yourId').value = JSON.stringify(data)
+	})
+
+	document.getElementById('connect').addEventListener('click', function(){
+		var otherId = JSON.parse(document.getElementById('otherId').value)
+		peer.signal(otherId)
+	})
+
+	document.getElementById('send').addEventListener('click', function (){
+		var yourMessage = document.getElementById('yourMessage').value
+		peer.send(yourMessage)
+	})
+
+	peer.on('data', function(data){
+		document.getElementById('messages').textContent += data + '\n'
+	})
+
+	peer.on('stream', function(stream){
+		var video = document.createElement('video')
+		document.body.appendChild(video)
+		
+		video.src = window.URL.createObjectURL(stream)
+		video.play()
+	})
+}, function(err){
+	console.error(err)
+})
