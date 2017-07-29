b5702e95e0b72671b0088ece6cb942b3cbf5151c
Chanjung Kim <fman1335@gmail.com>
2017-07-29 15:35:36 +0900

    update

---
+++ study/algorithm2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>세종대 스터디 모임</title>
+    </head>
+        <h1>세종대 스터디 모임</h1>
+        <h4>모임장: 김영우</h4>
+        <h4>멤버</h4>
+        <select name='fruits' size='6'>
+            <option value='' selected>-- 선택 --</option>
+            <option value='apple'>김영우</option>
+            <option value='banana'>김성곤</option>
+            <option value='lemon'>김찬중</option>
+            <option value='lemon'>지소현</option>
+            <option value='lemon'>지소현</option>
+            <option value='lemon'>지소현</option>
+        </select>
+    <body>
+    </body>
+</html>
