3889de96e954e10aba9a696f0acf475f0c61f54d
Chanjung Kim <chanjungskim@gmail.com>
2017-09-29 21:22:33 +0900

    start web project

---
+++ bogobogo/index.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+<title></title>
+</head>
+<script type="javascript">
+</script>
+<body>
+<fieldset>
+  <legend>Hello, World!</legend>
+  <table>
+    <tr>
+      <td><label>아이디</label></td>
+      <td><input type="text"></td>
+    </tr>
+    <tr>
+      <td><label>비밀번호</label></td>
+      <td><input type="password"></td>
+    </tr>
+    <tr>
+      <td colspan="2"><a href="#"><button>로그인</button></a></td>
+    </tr>
+  </table>
+</fieldset>
+</body>
+</html>
