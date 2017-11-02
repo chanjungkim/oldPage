9da64cc1424f40e3e88fa9316de1ea6ede8a914f
Chanjung Kim <chanjungskim@gmail.com>
2017-11-02 10:17:37 +0900

    ddff

--- study/androidstudy/eclipse-workspace/ATalk/src/logIn/Profile.java
+++
@@ -1,8 +0,0 @@
-package logIn;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Profile extends JFrame{
-	private JPanel panel = new JPanel();
-}
--- study/androidstudy/eclipse-workspace/ATalk/src/logIn/RoomList.java
+++
@@ -1,8 +0,0 @@
-package logIn;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class RoomList extends JFrame{
-	private JPanel panel = new JPanel();
-}
