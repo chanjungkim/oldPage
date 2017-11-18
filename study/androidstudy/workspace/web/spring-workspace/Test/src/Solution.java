126a9339e1b4072c6793a76c40dba424f0d04f22
chanjungkim <chanjungskim@gmail.com>
2017-11-18 17:36:07 +0900

    백준

--- study/androidstudy/workspace/web/spring-workspace/Test/src/Solution.java
+++ study/androidstudy/workspace/web/spring-workspace/Test/src/Solution.java
@@ -1,26 +1,30 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedHashSet;
-import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder("");
+		Scanner sc = new Scanner(System.in);
 		
-		LinkedHashSet<Short> list = new LinkedHashSet<>();
+		long min = sc.nextLong();
+		long max = sc.nextLong();
 		
-		while(st.hasMoreTokens()) {
-			Short s = Short.parseShort(st.nextToken());
-
-			if(!list.contains(s)) {
-				list.add(s);
-				sb.append(s+" ");
-			}
+		sc.nextLine();
+		
+		HashSet<Long> list = new HashSet<>();
+		
+		for(long i = 2 ; i <= Math.sqrt(max) ; i++) {
+			list.add((long)Math.pow(i, 2));
 		}
-		System.out.println(sb);
-		br.close();
+//		for(long l : list) {
+//			System.out.println(l);
+//		}
+		long res = max - min + 1 - list.size();
+
+		if(min < 2) {
+			res--;
+		}	
+		
+		System.out.println(res);
 	}
 }
---
+++ study/androidstudy/workspace/web/spring-workspace/Test/src/Solution2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution2 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.nextLine();
+		int i = 0x+str;
+		System.out.println("");
+////		String encoded = URLEncoder.encode(sc.nextLine(), "UTF-8");
+//		
+////		for (int i = 0; i < string.length(); i++) {
+////			System.out.print(String.format("U+%04X ", string.codePointAt(i)));
+////		}
+//		System.out.println(num);
+//		System.out.println(Integer.toHexString(num));
+//		System.out.format("%X\n", 123);
+//		System.out.format("%X\n", 235);
+//		System.out.format("%X\n", 236);
+	}
+}
